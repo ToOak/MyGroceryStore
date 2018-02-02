7ed96dd15a783b8cf5a6df9808e142e39364a07e
shuailong <xushuailong@111.com.cn>
2018-02-02 13:49:44 +0800

    初步提交

---
+++ app/src/main/java/com/example/xushuailong/mygrocerystore/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xushuailong.mygrocerystore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
