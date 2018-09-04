bb346ddb297ef6d9288be8c6a7bdee0b1a4207bf
shuailong <xuafeiJelly@163.com>
2018-09-04 17:49:13 +0800

    update

---
+++ app/src/main/java/com/example/xushuailong/mygrocerystore/bean/TestSerializable.java
@@ -0,0 +1,8 @@
+package com.example.xushuailong.mygrocerystore.bean;
+
+import java.io.Serializable;
+
+public class TestSerializable implements Serializable {
+
+    private static final long serialVersionUID = 532790020166852937L;
+}
